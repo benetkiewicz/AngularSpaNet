230c0a8f05e1b777dd6d4792c430f65d43b0d254
Piotr Benetkiewicz <piotr@ais.pl>
2014-07-26 23:06:18 +0200

    change app bundle to auto include

--- AngularSPA/App_Start/BundleConfig.cs
+++ AngularSPA/App_Start/BundleConfig.cs
@@ -9,10 +9,7 @@
             bundles.Add(new Bundle("~/Content/css")
                 .Include("~/Content/bootstrap.min.css")
                 .Include("~/Content/style.css"));
-            bundles.Add(new Bundle("~/scripts/application")
-                .Include("~/scripts/app/app.js")
-                .Include("~/scripts/app/controllers/registrationController.js")
-                .Include("~/scripts/app/services/carMakeService.js"));
+            bundles.Add(new Bundle("~/scripts/application").IncludeDirectory("~/scripts/app", "*.js", true));
             bundles.Add(new Bundle("~/scripts/libraries")
                 .Include("~/scripts/jquery-{version}.js")
                 .Include("~/scripts/angular.js")
