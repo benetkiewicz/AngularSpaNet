f730c7b8e7b02a59c1aac0e4f476ad6ea2dbdefb
Piotr Benetkiewicz <piotr@ais.pl>
2014-07-29 15:04:07 +0200

    add localized distance km/mi on summary

---
+++ AngularSPA/scripts/app/directives/localizedDistance.js
@@ -0,0 +1,13 @@
+﻿app.directive("localizeddistance", function () {
+    return {
+        restrict: "E",
+        link: function(scope, element, attrs) {
+            var unit = "km";
+            if (attrs.location === "A") {
+                unit = "mi";
+            }
+
+            element.text(attrs.distance + " " + unit);
+        }
+    };
+});
