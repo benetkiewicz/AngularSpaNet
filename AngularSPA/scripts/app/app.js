f633b1801c59ffe3ff9f16c477ff11a7f367efba
Piotr Benetkiewicz <piotr@ais.pl>
2014-08-21 15:06:33 +0200

    rewrite to controllerAs

--- AngularSPA/scripts/app/app.js
+++ AngularSPA/scripts/app/app.js
@@ -3,16 +3,19 @@
         'use strict';
         $routeProvider.when("/step1", {
             controller: "RegistrationCtrl",
+            controllerAs: "registration",
             templateUrl: "/scripts/app/views/step1.html"
         });
 
         $routeProvider.when("/step2", {
             controller: "RegistrationCtrl",
+            controllerAs: "registration",
             templateUrl: "/scripts/app/views/step2.html"
         });
 
         $routeProvider.when("/step3", {
             controller: "RegistrationCtrl",
+            controllerAs: "registration",
             templateUrl: "/scripts/app/views/step3.html"
         });
 
--- AngularSPA/scripts/app/controllers/registrationController.js
+++ AngularSPA/scripts/app/controllers/registrationController.js
@@ -1,45 +1,49 @@
 ﻿(function() {
     angular
         .module('registration')
-        .controller('RegistrationCtrl', [
-            '$scope', '$location', 'carMakeService', function($scope, $location, carMakeService) {
-                var steps = ['step1', 'step2', 'step3'];
-                $scope.currentStep = 0;
-                $scope.viewModel = {};
-                $scope.submitted = false;
-                $scope.locations = [{ id: 'E', name: 'Europe' }, { id: 'A', name: 'America' }];
+        .controller('RegistrationCtrl', Registration);
+    Registration.$inject = ['$location', 'carMakeService'];
 
-                $scope.nextStep = function(form) {
-                    $scope.submitted = true;
-                    if (form.$valid) {
-                        $scope.currentStep++;
-                        $location.path(steps[$scope.currentStep]);
-                        $scope.submitted = false;
-                    }
-                };
+    function Registration($location, carMakeService) {
+        var registration = this;
 
-                $scope.prevStep = function() {
-                    $scope.currentStep--;
-                    $location.path(steps[$scope.currentStep]);
-                };
+        var steps = ['step1', 'step2', 'step3'];
+        registration.currentStep = 0;
+        registration.viewModel = {};
+        registration.submitted = false;
+        registration.locations = [{ id: 'E', name: 'Europe' }, { id: 'A', name: 'America' }];
 
-                $scope.isLastStep = function() {
-                    return $scope.currentStep == steps.length - 1;
-                };
-
-                $scope.getCurrentStep = function() {
-                    return steps[$scope.currentStep];
-                };
-
-                $scope.updateCarMakes = function() {
-                    carMakeService.getCarMakes($scope.viewModel.location.id).success(function(data) {
-                        $scope.carMakes = data;
-                    });
-                };
-
-                $scope.submit = function() {
-                    alert('Submitted!');
-                };
+        registration.nextStep = function(form) {
+            registration.submitted = true;
+            if (form.$valid) {
+                registration.currentStep++;
+                $location.path(steps[registration.currentStep]);
+                registration.submitted = false;
             }
-        ]);
+        };
+
+        registration.prevStep = function() {
+            registration.currentStep--;
+            $location.path(steps[registration.currentStep]);
+        };
+
+        registration.isLastStep = function() {
+            return registration.currentStep == steps.length - 1;
+        };
+
+        registration.getCurrentStep = function() {
+            return steps[registration.currentStep];
+        };
+
+        registration.updateCarMakes = function() {
+            carMakeService.getCarMakes(registration.viewModel.location.id).success(function (data) {
+                console.dir(data);
+                registration.carMakes = data;
+            });
+        };
+
+        registration.submit = function() {
+            alert('Submitted!');
+        };
+    }
 })();
