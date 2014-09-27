fb6e444365ade4754b82bf1c507aeff331244eb0
Piotr Benetkiewicz <piotr@ais.pl>
2014-09-27 12:06:05 +0200

    add first test of car service

---
+++ AngularSPA/scripts/app/specs/servicesSpecs.js
@@ -0,0 +1,27 @@
+﻿///<reference path="~/scripts/jasmine.js"/>
+///<reference path="~/scripts/angular.js"/>
+///<reference path="~/scripts/angular-route.js"/>
+///<reference path="~/scripts/angular-mocks.js"/>
+
+///<reference path="~/scripts/App/app.js"/>
+///<reference path="~/scripts/App/services/carManufacturersService.js"/>
+describe("carManufacturerService tests", function() {
+    var $httpBackend, manufacturersService;
+    beforeEach(function() {
+        module('registration');
+        inject(function ($injector) {
+            $httpBackend = $injector.get('$httpBackend');
+            manufacturersService = $injector.get('carManufacturersService');
+        });
+
+    });
+
+    it('should return some regions', function() {
+        $httpBackend.when("GET", "/api/CarManufacturers/").respond([{ "Id": "E", "Name": "Europe" }, { "Id": "A", "Name": "America" }]);
+        manufacturersService.getWorldRegions().then(function (data) {
+            expect(data.data.length).toBeGreaterThan(0);
+        });
+        
+        $httpBackend.flush();
+    });
+});
